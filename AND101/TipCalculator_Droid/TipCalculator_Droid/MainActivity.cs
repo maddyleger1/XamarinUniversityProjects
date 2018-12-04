abea0f4649d984404b176802d0b3966b8f48dfaa
Maddy Leger <maddy@Maddys-MBP.guest.corp.microsoft.com>
2018-12-04 14:52:01 -0500

    completed tip calculator for android

--- AND101/TipCalculator_Droid/TipCalculator_Droid/MainActivity.cs
+++ AND101/TipCalculator_Droid/TipCalculator_Droid/MainActivity.cs
@@ -1,23 +1,40 @@
 ﻿using System;
 using Android.App;
 using Android.OS;
-using Android.Runtime;
-using Android.Support.Design.Widget;
 using Android.Support.V7.App;
-using Android.Views;
 using Android.Widget;
 
 namespace TipCalculator_Droid
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
-    public class MainActivity : Activity
+    public class MainActivity : AppCompatActivity
     {
 
-        protected override void OnCreate(Bundle bundle)
+        protected override void OnCreate(Bundle savedInstanceState)
         {
-            base.OnCreate(bundle);
+            base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
+
+            inputBill = FindViewById<EditText>(Resource.Id.inputBill);
+            calculateButton = FindViewById<Button>(Resource.Id.calculateButton);
+            outputTip = FindViewById<TextView>(Resource.Id.outputTip);
+            outputTotal = FindViewById<TextView>(Resource.Id.outputTotal);
+
+            calculateButton.Click += OnCalculateClick;
         }
-	}
+
+        void OnCalculateClick(object sender, EventArgs e)
+        {
+            var tip = double.Parse(inputBill.Text) * .15;
+            outputTip.Text = tip.ToString();
+            outputTotal.Text = (double.Parse(inputBill.Text) + tip).ToString();
+
+        }
+
+        EditText inputBill;
+        Button calculateButton;
+        TextView outputTip;
+        TextView outputTotal;
+    }
 }
 
