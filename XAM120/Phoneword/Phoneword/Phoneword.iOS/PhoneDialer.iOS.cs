9c7e69f2c8295882314d9d7f27b3f9b5fc9df654
Maddy Leger <maleger@microsoft.com>
2018-12-06 13:52:03 -0500

    XAM120

---
+++ XAM120/Phoneword/Phoneword/Phoneword.iOS/PhoneDialer.iOS.cs
@@ -0,0 +1,23 @@
+using System.Threading.Tasks;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+
+using Phoneword.iOS;
+
+// This same attribute definition needs to go in each platform-specific file
+[assembly: Dependency(typeof(PhoneDialer))]
+
+namespace Phoneword.iOS
+{
+	public class PhoneDialer : IDialer
+	{
+		public Task<bool> DialAsync(string number)
+		{
+			return Task.FromResult(
+				UIApplication.SharedApplication.OpenUrl(
+				new NSUrl("tel:" + number))
+			);
+		}
+	}
+}
